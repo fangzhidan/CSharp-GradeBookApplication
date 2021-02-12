3a94646474c7a0ae8bcfbe99fd1ef8d3bb1e35bc
fangzhidan <fangzhidan@gmail.com>
2021-02-12 19:28:22 +0000

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -54,5 +54,29 @@ namespace GradeBook.GradeBooks
             // Otherwise return 'F'
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < minNumberStudents)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < minNumberStudents)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
