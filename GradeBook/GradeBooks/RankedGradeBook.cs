9913126cc13b20a89fcd9be26a93c64974900747
fangzhidan <fangzhidan@gmail.com>
2021-02-12 16:23:20 +0000

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,63 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        private const int minNumberStudents = 5;
+        private const double twentyPercent = 0.2;
+
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < minNumberStudents)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            // Number of students per grade
+            int numStudentsPerGrade = (int)Math.Floor(Students.Count * twentyPercent);
+
+            // Sorted list of average grades (desc)
+            var avgGrades = Students.Select(s => s.AverageGrade).OrderByDescending(a => a);
+
+            // Identify letter grade
+            int indexAvgGrade = avgGrades.ToList().IndexOf(averageGrade);
+
+            // If in top 20%, award 'A'
+            if (indexAvgGrade < numStudentsPerGrade * 1)
+            {
+                return 'A';
+            }
+            // If in top 40%, award 'B'
+            else if (indexAvgGrade < (numStudentsPerGrade * 2))
+            {
+                return 'B';
+            }
+            // If in top 60%, award 'C'
+            else if (indexAvgGrade < (numStudentsPerGrade * 3))
+            {
+                return 'C';
+            }
+            // If in top 80%, award 'D'
+            else if (indexAvgGrade < (numStudentsPerGrade * 4))
+            {
+                return 'D';
+            }
+            // If in top 100%, award 'E'
+            else if (indexAvgGrade < (numStudentsPerGrade * 5))
+            {
+                return 'E';
+            }
+
+            // Otherwise return 'F'
+            return 'F';
         }
     }
 }
